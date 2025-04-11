e8ac6e76bbc8e4540eb4dbc1318158bdb6d5dc9c
HenilCrest <henil.m@crestinfosystems.com>
2025-04-11 17:34:55 +0530

    Updated the files(index.js, server.js) for deployment

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+// server.js or app.js
 const express = require("express");
-const http = require("http");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 
+dotenv.config();
+
 const app = express();
 app.use(cors());
-dotenv.config({ path: "./.env" });
 
 // Connect to MongoDB
 connectDB();
@@ -18,10 +19,4 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/user", userRoutes);
 
-const server = http.createServer(app);
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log("Connection established");
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
