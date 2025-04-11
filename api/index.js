ab3c6b83c4e3c82f63b83d17c8b186c378727ac0
HenilCrest <henil.m@crestinfosystems.com>
2025-04-11 17:09:51 +0530

    Added vercel.json and installed other packages into it

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("../config/db");
+const authRoutes = require("../routes/auth.routes");
+const userRoutes = require("../routes/user.routes");
+const serverless = require("serverless-http");
+
+const app = express();
+app.use(cors());
+dotenv.config({ path: "./.env" });
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use("/api", authRoutes);
+app.use("/user", userRoutes);
+
+const server = http.createServer(app);
+
+module.exports.handler = serverless(app);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log("Connection established");
+  console.log(`Server is running on port ${PORT}`);
+});
