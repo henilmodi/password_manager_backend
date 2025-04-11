48b2bc894b6db6929dccd58872b3c769921f70d3
HenilCrest <henil.m@crestinfosystems.com>
2025-04-11 17:42:36 +0530

    Updated the db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,30 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
   try {
-    await mongoose
-      .connect(process.env.MONGO_URI)
-      .then(() => console.log("MongoDB Connected"))
-      .catch((err) => console.log("MongoDB connection error: ", err));
+    cached.conn = await cached.promise;
+    console.log("✅ MongoDB Connected");
+    return cached.conn;
   } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err);
-    process.exit(1);
+    console.error("❌ MongoDB connection error:", err);
+    throw err;
   }
 };
 
